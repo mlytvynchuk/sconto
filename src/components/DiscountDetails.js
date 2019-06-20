512acfa712e68ed2decb2e96642d3aa16a02d545
Riaboshtan <sasha.riaboshtan@gmail.com>
2019-06-20 21:54:46 +0300

    map

--- src/components/DiscountDetails.js
+++ src/components/DiscountDetails.js
@@ -3,38 +3,44 @@ import '../assets/css/discount-details.css'
 import coupon from '../assets/img/disc.png'
 import grad from '../assets/img/top.png'
 
-const DiscountDetails = (props) => {
-    
-    return (
-        <div>
-            <div className="image-container">
-                <img className="img1" src={props.image} alt=""/>
-                <img className="img2" src={grad} alt="gradient"/>
-            </div>
+class DiscountDetails extends React.Component {
 
-            <div className="cafe">
-                <h2>{props.cafe}</h2>
-            </div>
-            <div className="description">
-                <div className="title">
-                    <img src={coupon} alt="coupon"/>
-                  <h3>{props.title}</h3>
+    raiseInvoiceClicked(){
+        const url = 'http://localhost:3000/map';
+        window.open(url, '_blank');
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="image-container">
+                    <img className="img1" src={this.props.image} alt=""/>
+                    <img className="img2" src={grad} alt="gradient"/>
                 </div>
 
-                <div className="details">
-                    <h3>Детальна інформація</h3>
-                    <p>{props.details}</p>
+                <div className="cafe">
+                    <h2>{this.props.cafe}</h2>
                 </div>
+                <div className="description">
+                    <div className="title">
+                        <img src={coupon} alt="coupon"/>
+                    <h3>{this.props.title}</h3>
+                    </div>
 
-                <div className="map-container">
-                    <button className="btn" > <a href="http://localhost:3000/map" /></button>
-                    {/*<img src={loc} alt="loc" />*/}
-                    <p>{props.location}</p>
+                    <div className="details">
+                        <h3>Детальна інформація</h3>
+                        <p>{this.props.details}</p>
+                    </div>
+
+                    <div className="map-container">
+                        <button className="btn" onClick={this.raiseInvoiceClicked}> </button>
+                        <p>{this.props.location}</p>
+                    </div>
                 </div>
+                    <button className="btn-like" onClick={this.props.onAddedToLikes}> Like </button>
             </div>
-                <button className="btn-like" onClick={props.onAddedToLikes}> Like </button>
-        </div>
-    )
+        )
+    }
 }
 
 export default DiscountDetails;
